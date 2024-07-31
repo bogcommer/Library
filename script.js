4ad305de5db5495b0f44766973ba9d6da2e8b146
bogcommer <beddoeschristian@gmail.com>
2024-07-31 10:34:01 +0100

    Changed to class

--- script.js
+++ script.js
@@ -10,12 +10,15 @@ const removeData = document.querySelector(".remove-book");
 let mockDataAdded = false;
 let start = 0;
 
-function Book(id, title, author, pages, read) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+
+    constructor(id, title, author, pages, read) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(id, title, author, pages, read) {
